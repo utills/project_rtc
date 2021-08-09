9c59b66bb8e83d95dc4c4815a9605a4b50fb22f9
Vivek Singh <ervivek40@gmail.com>
2021-08-09 08:35:12 -0700

    vrsfg

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -40,8 +40,34 @@
     app.factory('camera', ['$rootScope', '$window', function($rootScope, $window){
     	var camera = {};
     	camera.preview = $window.document.getElementById('localVideo');
+		function shimGetDisplayMedia(window, preferredMediaSource) {
+			console.log("window",window);
+			console.log("window.navigator",window.navigator);
+			console.log("window.navigator.mediaDevices",window.navigator.mediaDevices);
+		if (window.navigator.mediaDevices && 'getDisplayMedia' in window.navigator.mediaDevices) {
+		  return;
+		}
+		if (!window.navigator.mediaDevices) {
+		  return;
+		}
+		window.navigator.mediaDevices.getDisplayMedia = function getDisplayMedia(constraints) {
+		  if (!(constraints && constraints.video)) {
+			var err = new DOMException('getDisplayMedia without video ' + 'constraints is undefined');
+			err.name = 'NotFoundError';
+			// from https://heycam.github.io/webidl/#idl-DOMException-error-names
+			err.code = 8;
+			return Promise.reject(err);
+		  }
+		  if (constraints.video === true) {
+			constraints.video = { mediaSource: preferredMediaSource };
+		  } else {
+			constraints.video.mediaSource = preferredMediaSource;
+		  }
+		  return window.navigator.mediaDevices.getUserMedia(constraints);
+		};
+	  }
     	camera.start = function(){
-			return app.shimGetDisplayMedia($window,mediaConfig)
+			return shimGetDisplayMedia($window,mediaConfig)
 			.then(function(stream){			
 				attachMediaStream(camera.preview, stream);
 				client.setLocalStream(stream);
